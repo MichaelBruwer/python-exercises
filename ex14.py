fa0715e2fe38f801c7b658a1b9bdca61770de8ba
MichaelBruwer <michaelbruwer21@gmail.com>
2020-10-16 14:25:24 +0200

    first commit

--- ex14.py
+++
@@ -1,9 +0,0 @@
-def gargle(meh):
-  bleh = 0
-
-  for meh in meh:
-    if len(meh) > 1 and meh[0] == meh[-1]:
-      bleh += 1
-  return bleh
-
-print(gargle(['alabama', 'toiletpaper', 'spartans', '404']))
